c7f034039f1213934aba706f5e9a6af6467ff698
miaoran <1822773181@qq.com>
2015-04-25 19:49:20 +0800

    1:项目初始化

---
+++ QQ5.0消息提示/QQ5.0消息提示/class/controller/MRViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRViewController.h
+//  QQ5.0消息提示
+//
+//  Created by admin on 15/4/25.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRViewController : UIViewController
+
+@end
---
+++ QQ5.0消息提示/QQ5.0消息提示/class/other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ5.0消息提示
+//
+//  Created by admin on 15/4/25.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
